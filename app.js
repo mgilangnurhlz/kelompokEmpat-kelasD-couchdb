e26482bb2eae21d2fc736fb14e8cd57e48820ca8
Muhammad Gilang Nur Haliz <mgilangnurhaliz@gmail.com>
2022-12-16 20:00:21 +0700

    adding static file

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ couch.listDatabases().then(function (dbs) {
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
